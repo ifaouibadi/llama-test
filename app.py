d8c93bf04728bd572e32c398ebfcda342bbcd56a
Badi Ifaoui <badi1wow@gmail.com>
2023-10-07 11:53:13 +0100

    feat: basic ex

--- app.py
+++ app.py
@@ -1,43 +1,61 @@
-from dotenv import load_dotenv
-from langchain import HuggingFaceHub, LLMChain
-from langchain.prompts import PromptTemplate
 import streamlit as st
+from dotenv import load_dotenv
+from langchain import HuggingFaceHub
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import Chroma
+
+from constants import PERSIST_DIRECTORY
 
 load_dotenv()
 
 hub_llm = HuggingFaceHub(
-    # repo_id="gpt2",
-    model_kwargs={"max_length": 4000, "temperature": 0.6},
+    # model_kwargs={"max_length": 4000, "temperature": 0.6},
     repo_id="meta-llama/Llama-2-7b-chat-hf",
-    # repo_id="mrm8488/t5-base-finetuned-wikiSQL",
 )
+
+system_prompt = """You are a helpful assistant, you will use the provided context to answer user questions.
+Read the given context before answering questions and think step by step. If you can not answer a user question based on 
+the provided context, inform the user. Do not use any other information for answering user. Provide a detailed answer to the question."""
+
+instruction = """
+            Context: {context}
+            User: {question}"""
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+SYSTEM_PROMPT = B_SYS + system_prompt + E_SYS
+prompt_template = B_INST + SYSTEM_PROMPT + instruction + E_INST
+
 
 prompt = PromptTemplate(
-    input_variables=["question"],
-    template="""
-    You're a help full assistance agent for GuardMe,
-    GuardMe is a solutions-focused health and wellness insurance provider for international students, staff, and schools. Our dedicated team understands the exciting journey international students are on, and how an unexpected medical bill or mental health concern can stop it. No matter the situation, we care to look for solutions.
-We empower students, schools, and support staff, to safely travel the world to enrich their lives through international study. We believe everyone should feel safe, supported, and cared for during their time abroad. What sets  apart is that we genuinely care, that’s why we’ve created multiple solutions to ensure you’re taken care of in any situation.
-
-
-
-    answer the customers question: {question}
-    """,
+    input_variables=["context", "question"],
+    template=prompt_template,
 )
 
 
-hub_chain = LLMChain(
-    prompt=prompt,
+embeddings = HuggingFaceEmbeddings()
+db = Chroma(
+    embedding_function=embeddings,
+    persist_directory=PERSIST_DIRECTORY,
+)
+
+retriever = db.as_retriever()
+
+
+qa = RetrievalQA.from_chain_type(
     llm=hub_llm,
+    chain_type="stuff",
     verbose=True,
+    retriever=retriever,
+    chain_type_kwargs={"prompt": prompt},
+    # chain_type_kwargs={"prompt": prompt, "memory": memory},
 )
 
 
 text = st.text_area("enter some text")
 
 if text:
-    out = hub_chain.run(text)
-    st.json({
-        "input": text,
-        "output": out
-    })
+    out = qa.run(text)
+    st.json({"input": text, "output": out})
