36dc46b65150e9560c1fd7a7bc02eae478dbcc6f
Badi Ifaoui <ifaouibadi@users.noreply.huggingface.co>
2023-10-07 07:47:16 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from transformers import pipeline
+
+pipe = pipeline('sentiment-analysis')
+
+text = st.text_area('enter some text')
+
+if text:
+    out = pipe(text)
+    st.json(out)
